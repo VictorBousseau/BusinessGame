671edbee968aa36cc0f1bb8f946afb166a8bc342
Hélène LOTTIN <bousseauvictor49@gmail.com>
2024-12-07 16:01:26 +0100

    correctif 1

--- start_app.bat
+++ start_app.bat
@@ -1,4 +1,7 @@
 @echo off
-start /B py start_flask.py
-timeout /t 2
-start https://victorbousseau.github.io/BusinessGame/
+echo Démarrage du serveur Flask...
+start cmd /K ""C:\Users\helen\anaconda3\python.exe" "C:\Users\helen\OneDrive\Bureau\Victor Test\BusinessGame\start_flask.py""
+timeout /t 5
+echo Ouverture du site web...
+start "" "https://victorbousseau.github.io/BusinessGame/"
+pause
