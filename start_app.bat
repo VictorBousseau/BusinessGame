4e80ae1a6cacebcab776a08f27453eec8a7b41f1
VictorBousseau <153196602+VictorBousseau@users.noreply.github.com>
2024-12-19 11:02:37 +0100

    Update start_app.bat

--- start_app.bat
+++ start_app.bat
@@ -1,7 +1,7 @@
 :: Fichier pour le démarage de l'application, double clic sur ce fichier, cela lancera le programme start_flask.py et amenera sur l'URL :https://victorbousseau.github.io/BusinessGame/ 
 @echo off
 echo Démarrage du serveur Flask...
-start cmd /K ""C:\Users\helen\anaconda3\python.exe" "C:\Users\helen\OneDrive\Bureau\Victor Test\BusinessGame\start_flask.py""
+start cmd /K py start_flask.py
 timeout /t 5
 echo Ouverture du site web...
 start "" "https://victorbousseau.github.io/BusinessGame/"
